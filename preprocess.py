5094f66ba96e9211ee754ec4ccb2c28f380192cb
Danelle Cline <dcline@mbari.org>
2017-07-22 13:47:22 -0700

    Added high/low cut off as paramters, refactored preprocessing function for blue and fin calls, and use bled detection table Label to save spectrogram images into directories by Label

--- preprocess.py
+++ preprocess.py
@@ -2,57 +2,79 @@ import matplotlib.pyplot as plt
 import spectrogram_utilities
 import soundfile as sf
 import os
-  
+import glob
+import pandas as pd
+import conf
+ 
 
-def plot_spectrogram(date, audio_path, spectrogram_out=None, bin_size=2 ** 10):
+def preprocess(bled_path, spectrogram_path, low_cut_off_freq, high_cut_off_freq, num_fft):
     '''
-     plot spectrogram
-    :param audio_path: location of original audio file (.wav)
-    :param spectrogram_out: directory where all produced spectrograms are located
-    :param bin_size: audio file's sample rate (Hz)
-    :return: saves a spectrogram in spectrogram_out and displays it
-    '''
+    Generate spectrograms of all sounds recursively in a directory
+    First search for the txt that describes the annotation - use this to sort the spectrograms into
+    separate directories for classification
+    
+    :param bled_path:  BLED detection path
+    :param spectrogram_path:  directory where all produced spectrograms are stored
+    :param low_cut_off_freq: 
+    :param high_cut_off_freq: 
+    :param num_fft: size of fft 
+    '''  
+    
+    for full_filename in glob.iglob(bled_path + '**/*.txt', recursive=True):
+        print('Reading {0}'.format(full_filename))
+        df = pd.read_csv(full_filename, sep='\t')
+        if 'Selection' in df.keys():
+            path, file = os.path.split(full_filename)
+            date = path.rsplit('/BLED', 1)[-1]
+            for wav in glob.iglob(path + '**/*.wav', recursive=False):
 
-    info = sf.info(audio_path)
-    sample_rate = info.samplerate
-    frames = info.frames
+                # get the selection number in the filename
+                wav_path, wav_file = os.path.split(wav)
+                selection = int(wav_file.split('.')[1])
 
-    with sf.SoundFile(audio_path, 'r') as f:
-        _samples = f.read(frames)
-        out_file = ''
-        if spectrogram_out:
-            _, file = os.path.split(audio_path)
-            base_file = file.split('.wav')[0]
-            out_file = os.path.join(spectrogram_out, base_file + date + '.spectrogram.png')
+                # lookup class by selection number
+                match = df[df.Selection == selection]
+                label = match.iloc[0].Label
+                spectrogram_path_by_class = os.path.join(spectrogram_path, label)
 
-        spectrogram_utilities.optimize_spectrogram(_samples, sample_rate, binsize=bin_size, plotpath=out_file)
-        # uncomment below to display instead of just saving to disk
-        #spectrogram_utilities.display_optimized_spectrogram(_samples, sample_rate, binsize=bin_size, plotpath=out_file)
+                if not os.path.exists(spectrogram_path_by_class):
+                    os.mkdir(spectrogram_path_by_class)
+ 
+                info = sf.info(wav_path)
+                sample_rate = info.samplerate
+                frames = info.frames
 
+                with sf.SoundFile(wav_path, 'r') as f:
+                    _samples = f.read(frames) 
+                    _, file = os.path.split(wav_path)
+                    base_file = file.split('.wav')[0]
+                    out_file = os.path.join(spectrogram_path_by_class, base_file + date + '.spectrogram.png')
+
+                    spectrogram_utilities.optimize_spectrogram(low_cut_off_freq, high_cut_off_freq, _samples, sample_rate,
+                                                               binsize=num_fft, plotpath=out_file)
+                    # uncomment below to display instead of just saving to disk
+                    # spectrogram_utilities.display_optimized_spectrogram(_samples, sample_rate, binsize=bin_size, plotpath=out_file)
+                 
 
 if __name__ == '__main__':
 
-    # Set path to directory with folders train and test wav files
-    path_data = '/Users/ddeleon/Google Drive/MBARI/PAM_Summer_Project_2017/BLED_Results/'
+    # Set path to directory with folders train and test wav files 
+    blue_bled_path = '/Users/dcline/Google Drive/PAM_Summer_Project_2017/BLED_Results/BlueWhaleB/'
+    fin_bled_path = '/Users/dcline/Google Drive/PAM_Summer_Project_2017/BLED_Results/FinWhale/'
     
     # Set path to directory to save optimized spectrogram of wav files
-    #spectrogram_path = '/Users/ddeleon/Google Drive/MBARI/PAM_Summer_Project_2017/spectrograms'
-    spectrogram_path = '/Users/ddeleon/Documents/Bspectrograms'
+    blue_spectrogram_path = '/Users/dcline/Google Drive/PAM_Summer_Project_2017/data/BlueWhaleB/'
+    fin_spectrogram_path = '/Users/dcline/Google Drive/PAM_Summer_Project_2017/data/FinWhale/' 
 
     # make the directory if it doesn't exist
-    if not os.path.exists(spectrogram_path):
-        os.mkdir(spectrogram_path)
+    if not os.path.exists(blue_spectrogram_path):
+        os.mkdir(blue_spectrogram_path)
+    if not os.path.exists(fin_spectrogram_path):
+        os.mkdir(fin_spectrogram_path)
     
     # Plot whale sound
-    audio_path = os.path.join(path_data, 'BlueWhaleB/BLED20150914/') + 'bb_sel.01.ch01.170620.083556.91..wav' 
-    plot_spectrogram('20150914', audio_path, spectrogram_path, bin_size=250)
-    
-    # Generate spectrograms of all sounds recursively in a directory  
-    for root, dirs, files in os.walk(path_data):
-        for file in files:
-            if file.endswith(".wav"):
-                date = root.rsplit('/BLED',1)[-1]
-                audio_path = os.path.join(root, file)
-                plot_spectrogram(date, audio_path, spectrogram_path, 250)
-    
-     
+    #audio_path = os.path.join(path_data, 'BlueWhaleB/BLED20150914/') + 'bb_sel.01.ch01.170620.083556.91..wav'
+    #plot_spectrogram('20150914', audio_path, spectrogram_path, bin_size=250)
+
+    preprocess(fin_bled_path, fin_spectrogram_path, conf.fin_low_cut_off_freq, conf.fin_high_cut_off_freq, 512)
+    preprocess(blue_bled_path, blue_spectrogram_path, conf.blue_b_low_cut_off_freq, conf.blue_b_high_cut_off_freq, 256)
