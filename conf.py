6986d1e5aab50aaafe02fb1f942b423d250954cf
Danelle Cline <dcline@mbari.org>
2017-07-22 13:47:40 -0700

    Added high/low cut off as paramters

--- conf.py
+++ conf.py
@@ -1,3 +1,8 @@
+# Spectrogram parameters
+blue_b_low_cut_off_freq, blue_b_high_cut_off_freq = 39, 51
+fin_low_cut_off_freq, fin_high_cut_off_freq = 10, 39
+
+# Classification model parameters
 BOTTLENECK_TENSOR_NAME = 'pool_3/_reshape:0'
 BOTTLENECK_TENSOR_SIZE = 2048
 MODEL_INPUT_WIDTH = 299
